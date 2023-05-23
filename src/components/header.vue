516539e23c3c893e0f5d0a58060ffda7347d157b
ahmedghribi <ahmedghribi51@gmail.com>
2023-05-23 21:12:33 +0200

    add header

---
+++ src/components/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="#" class="flex items-center">
+        <img
+          src="https://www.editions-tissot.fr/wp-content/uploads/2020/10/logo-hcse.png"
+          class="h-8 mr-3"
+          alt="HelloCSE Logo"
+        />
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+/* Add your scoped styles here */
+</style>
