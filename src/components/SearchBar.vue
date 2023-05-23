39d82d0ebb83d07ce8d318dd84aaacfa0fd14586
ahmedghribi <ahmedghribi51@gmail.com>
2023-05-23 21:26:12 +0200

    Modifier le style du bouton

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,14 +2,34 @@
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <div class="grid grid-cols-3 gap-4 content-normal ...">
       <div>
-        <input
-          class="searchBar max-w-sm py-2 px-4 outline-none rounded-lg text-gray-900"
-          type="text"
-          v-model="query"
-          @keyup="getResult(query)"
-        />
+        <label for="simple-search" class="sr-only">Search</label>
+        <div class="relative w-full">
+          <div
+            class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+          >
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="text"
+            v-model="query"
+            @keyup="getResult(query)"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Recherche par titre"
+          />
+        </div>
       </div>
-      <div>02</div>
     </div>
   </div>
 </template>
@@ -33,6 +53,9 @@ function getResult(query) {
         emits("getResults", response.data.results);
       });
   }
+}
+function noteFilter() {
+  console.log("note");
 }
 </script>
 
